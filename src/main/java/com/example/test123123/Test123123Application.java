21ece9c09ceb67387cc8410588a30b92bbb54924
TorusTredent <torus.tredent@gmail.com>
2023-03-11 16:08:07 +0300

    init

---
+++ src/main/java/com/example/test123123/Test123123Application.java
@@ -0,0 +1,13 @@
+package com.example.test123123;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test123123Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test123123Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/test123123/Test123123ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test123123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test123123ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
